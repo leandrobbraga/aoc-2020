fc4fad961250ec5b76fbfe8c39bdb1754369fc39
Leandro Braga <leandrobbraga@gmail.com>
2020-12-07 17:20:31 -0300

    Small refactor in day-06 solution

--- examples/day-06.rs
+++ examples/day-06.rs
@@ -11,28 +11,28 @@ fn main() {
     println!("Part 02: {}", result_part_02);
 }
 
-fn solve(group_answers: &Vec<&str>) -> (u32, u32) {
-    let mut dict: HashMap<char, u32> = HashMap::new();
+fn solve(groups_answers: &Vec<&str>) -> (u32, u32) {
+    let mut answers_counter: HashMap<char, u32> = HashMap::new();
     let mut result_part_01: u32 = 0;
     let mut result_part_02: u32 = 0;
 
-    for group_answer in group_answers {
-        for answer in group_answer.chars() {
+    for group_answers in groups_answers {
+        for answer in group_answers.chars() {
             if answer != '\n' {
-                let counter: &mut u32 = dict.entry(answer).or_insert(0);
-                *counter += 1;
+                let answer_counter: &mut u32 = answers_counter.entry(answer).or_insert(0);
+                *answer_counter += 1;
             }
         }
 
-        result_part_01 += dict.len() as u32;
+        result_part_01 += answers_counter.len() as u32;
 
-        for value in dict.values() {
-            if *value == group_answer.lines().count() as u32 {
+        for value in answers_counter.values() {
+            if *value == group_answers.lines().count() as u32 {
                 result_part_02 += 1;
             }
         }
 
-        dict.clear();
+        answers_counter.clear();
     }
     (result_part_01, result_part_02)
 }
