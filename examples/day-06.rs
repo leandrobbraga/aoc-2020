f517f7012cccfb09d41b322ce96e9898758cbe17
Leandro Braga <leandrobbraga@gmail.com>
2020-12-07 17:18:50 -0300

    Marging day-06 part-01 and part-02 solution

--- examples/day-06.rs
+++ examples/day-06.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -6,32 +6,15 @@ fn main() {
 
     let group_answers: Vec<&str> = input.split("\n\n").collect();
 
-    let result = solve_part_01(&group_answers);
-    println!("{}", result);
-
-    let result = solve_part_02(&group_answers);
-    println!("{}", result);
+    let (result_part_01, result_part_02) = solve(&group_answers);
+    println!("Part 01: {}", result_part_01);
+    println!("Part 02: {}", result_part_02);
 }
 
-fn solve_part_01(group_answers: &Vec<&str>) -> usize {
-    let mut dict: HashSet<char> = HashSet::new();
-
-    let mut result: usize = 0;
-    for answers in group_answers {
-        for answer in answers.chars() {
-            if answer != '\n' {
-                dict.insert(answer);
-            }
-        }
-        result += dict.len();
-        dict.clear();
-    }
-    result
-}
-
-fn solve_part_02(group_answers: &Vec<&str>) -> usize {
+fn solve(group_answers: &Vec<&str>) -> (u32, u32) {
     let mut dict: HashMap<char, u32> = HashMap::new();
-    let mut result: usize = 0;
+    let mut result_part_01: u32 = 0;
+    let mut result_part_02: u32 = 0;
 
     for group_answer in group_answers {
         for answer in group_answer.chars() {
@@ -41,13 +24,15 @@ fn solve_part_02(group_answers: &Vec<&str>) -> usize {
             }
         }
 
+        result_part_01 += dict.len() as u32;
+
         for value in dict.values() {
             if *value == group_answer.lines().count() as u32 {
-                result += 1;
+                result_part_02 += 1;
             }
         }
 
         dict.clear();
     }
-    result
+    (result_part_01, result_part_02)
 }
