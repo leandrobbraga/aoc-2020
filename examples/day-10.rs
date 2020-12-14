26dab2a96096ddf1b0607a007f95ee50136eaa2d
Leandro Braga <leandrobbraga@gmail.com>
2020-12-14 17:59:38 -0300

    Draft solution for day-10

--- examples/day-10.rs
+++ examples/day-10.rs
@@ -1,25 +1,25 @@
+use std::cmp::max;
 use std::fs;
 
 fn main() {
     let input: String = fs::read_to_string("./examples/input/day-10.txt").unwrap();
     let mut adapters: Vec<u64> = input.lines().map(|line| line.parse().unwrap()).collect();
+    adapters.push(0);
     adapters.sort();
+    adapters.push(adapters[adapters.len() - 1] + 3);
 
-    solve_part_01(&adapters)
+    solve_part_01(&adapters);
+    solve_part_02(&adapters);
 }
 
 fn solve_part_01(adapters: &Vec<u64>) {
     let mut ones: u64 = 0;
-    let mut threes: u64 = 1; // We start at 1 because the difference to the device is always 3
+    let mut threes: u64 = 0;
 
     let mut jolts_difference;
 
-    for i in 0..adapters.len() {
-        if i == 0 {
-            jolts_difference = adapters[i];
-        } else {
-            jolts_difference = adapters[i] - adapters[i - 1];
-        }
+    for i in 1..adapters.len() {
+        jolts_difference = adapters[i] - adapters[i - 1];
 
         if jolts_difference == 1 {
             ones += 1;
@@ -28,5 +28,28 @@ fn solve_part_01(adapters: &Vec<u64>) {
         }
     }
 
-    println!("ones:{}, threes:{}, mul:{}", ones, threes, ones * threes)
+    println!(
+        "Part 01 - ones:{}, threes:{}, mul:{}",
+        ones,
+        threes,
+        ones * threes
+    )
+}
+
+fn solve_part_02(adapters: &Vec<u64>) {
+    let mut paths: Vec<u64> = vec![];
+
+    paths.push(1);
+
+    for i in 1..adapters.len() {
+        paths.push(0);
+        let lb = max(0, i as i64 - 3) as usize;
+        for j in lb..i {
+            if adapters[i] - adapters[j] <= 3 {
+                paths[i] += paths[j];
+            }
+        }
+    }
+
+    println!("Part 02 - Paths: {}", paths[adapters.len() - 1])
 }
